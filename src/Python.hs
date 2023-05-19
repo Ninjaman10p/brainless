e03781b2bf67b6aee284c3e2489f403bc22f892a
_boundless_recursion <boundless.recursion@gmail.com>
2023-05-19 17:55:13 +1000

    updated the tests

--- src/Python.hs
+++ src/Python.hs
@@ -1,24 +1,31 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes       #-}
+{-# LANGUAGE TemplateHaskell  #-}
 
 module Python (Instruction (..), Program, Expression (..), Variable, expr, variable, file, parsePython, instruction) where
 
 import           Control.Applicative
-import           Control.Arrow
+import           Control.Lens              hiding (noneOf)
 import           Control.Monad
 import           Control.Monad.State
-import           Control.Monad.State.Class
 import           Data.Foldable             (asum)
 import           Data.Functor
 import           Data.Text                 (Text)
 import qualified Data.Text                 as T
-import           Text.Parsec               hiding (State, many, space, spaces,
-                                            (<|>), optional)
+import           Text.Parsec               hiding (State, many, optional, space,
+                                            spaces, (<|>))
 
-type PythonParser a = ParsecT Text () (State (Int, Bool)) a
+type PythonParser a = ParsecT Text () (State ParserState) a
+
+data ParserState = PS
+  { _indentLevel  :: Int
+  , _isIndenting  :: Bool
+  , _inExpression :: Bool
+  }
+$(makeLenses 'PS)
 
 parsePython :: PythonParser a -> SourceName -> Text -> Either ParseError a
-parsePython p n s = evalState (runParserT p () n s) (0, False)
+parsePython p n s = evalState (runParserT p () n s) $ PS 0 False False
 
 data Expression = EVar Variable
                 | EString Text
@@ -60,21 +67,25 @@ int :: Stream s m Char => ParsecT s u m Int
 int = read <$> some digit
 
 space :: PythonParser Char
-space = oneOf " "
+space = oneOf " " <|> (string "\\\n" $> '\n')
+
+comment :: PythonParser String
+comment = string "#" *> manyTill anyChar endOfLine
 
 spaces :: PythonParser ()
-spaces = void $ many space
+spaces = void $ many (void comment <|> void space)
 
 factor :: PythonParser Expression
 factor = asum $ try <$>
   [ char '(' *> spaces *> expr <* spaces <* char ')'
+  , EOrd <$> (string "ord(" *> spaces *> expr <* spaces <* string ")")
+  , EChr <$> (string "chr(" *> spaces *> expr <* spaces <* string ")")
   , ENot <$> (string "not" *> spaces *> factor)
-  , EBool <$> ((string "True" $> True) <|> (string "False" $> False))
+  , EBool <$> (try (string "True" $> True) <|> (string "False" $> False))
   , ENum <$> int
-  , EVar <$> variable
   , EInput <$> (string "input(" *> optional expr <* string ")")
-  , EOrd <$> (string "ord(" *> expr <* string ")")
-  , EChr <$> (string "chr(" *> expr <* string ")")
+  , EString <$> stringLiteral
+  , EVar <$> variable
   ]
 
 expr :: PythonParser Expression
@@ -96,31 +107,43 @@ expr = do
     , return a
     ]
 
+stringLiteral :: PythonParser Text
+stringLiteral = fmap T.pack $
+  (try (string "\"\"\"") *> many ((string "\\\"" $> '"') <|> noneOf "\"") <* string "\"\"\"")
+  <|> (char '\'' *> many ((string "'" $> '\'') <|> noneOf "\"\n") <* char '\'')
+  <|> (char '"' *> many ((string "\\\"" $> '"') <|> noneOf "\"\n") <* char '"')
+
 file :: PythonParser [Instruction]
-file = many instruction <* spaces <* eof
+file = many (many (void comment  <|> void endOfLine) *> instruction) <* spaces <* eof
 
 indentation :: PythonParser ()
 indentation = do
-  (n, changing) <- get
+  PS {_indentLevel = n, _isIndenting = changing} <- get
   forM_ [1..n] (const space)
   when changing $ do
     newSpaces <- try $ length <$> some space
-    put (n + newSpaces, False)
+    modify $ over indentLevel (+newSpaces)
+    modify $ set isIndenting False
+
+lock :: MonadState s m => m a -> m a
+lock p = do
+  a <- get
+  p <* put a
 
 indented :: PythonParser a -> PythonParser a
 indented p = do
   _ <- endOfLine
-  n <- gets fst
-  modify (second (const True))
-  p <* put (n, False)
+  lock $ do
+    modify $ set isIndenting True
+    p
 
 instruction :: PythonParser Instruction
 instruction = (indentation *>) . asum $ try <$>
-    [ Set <$> variable <* spaces <* char '=' <* spaces <*> expr <* (void (some endOfLine) <|> eof)
+    [ Set <$> variable <* spaces <* char '=' <* spaces <*> expr <* eol
     , block "while" While
     , block "if" If
-    , Print <$> (string "print(" *> spaces *> expr <* spaces <* char ')')
-    ]
+    , Print <$> (string "print(" *> spaces *> expr <* spaces <* char ')') <* eol
+    ] where eol = spaces *> (void (some endOfLine) <|> eof)
 
 block :: String -> (Expression -> [Instruction] -> b) -> PythonParser b
 block s f = f <$> (string s *> spaces *> expr <* spaces <* char ':' <* spaces) <*> indented (some $ try instruction)
